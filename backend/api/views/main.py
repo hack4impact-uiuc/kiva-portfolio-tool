43af7899b9de0f206bf12ba655fdc0078e11ccbe
Daniel S Choi <daniel.won.choi@gmail.com>
2019-02-19 12:53:36 -0600

    added tests and modified messages.py (#12)
    * added tests and modified messages.py
    
    * is circleci broken
    
    * formatted
    
    * removed DS file
    
    * erased DS
    
    * quick formatting of the data

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from api.models import Person, Document
+from api.models import Person, Document, Message
 from api.core import create_response, serialize_list, logger
 
 main = Blueprint("main", __name__)  # initialize blueprint
@@ -20,6 +20,13 @@ def index():
 def get_persons():
     persons = Person.query.all()
     return create_response(data={"persons": serialize_list(persons)})
+
+
+# function that is called when you visit /persons
+@main.route("/messages", methods=["GET"])
+def get_messages():
+    messages = Message.query.all()
+    return create_response(data={"messages": serialize_list(messages)})
 
 
 # function that is called when you visit /documetns
