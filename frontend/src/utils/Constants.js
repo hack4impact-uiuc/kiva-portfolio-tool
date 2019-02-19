094f6c8a4edd449cb52767c403ccec1d7e0511c6
Arpan Laha <arpanlaha99@gmail.com>
2019-02-18 23:01:12 -0600

    add constants

---
+++ frontend/src/utils/Constants.js
@@ -0,0 +1,22 @@
+const Constants = {
+  statuses: [
+    { value: 'Missing' },
+    { value: 'Pending' },
+    { value: 'Rejected' },
+    { value: 'Verified' }
+  ],
+  types: [
+    { value: 'Type 1' },
+    { value: 'Type 2' },
+    { value: 'Type 3' },
+    { value: 'Type 4' },
+    { value: 'Type 5' },
+    { value: 'Type 6' },
+    { value: 'Type 7' },
+    { value: 'Type 8' },
+    { value: 'Type 19' },
+    { value: 'Type 1' }
+  ]
+}
+
+export default Constants
