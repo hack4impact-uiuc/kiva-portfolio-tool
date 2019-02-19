8b9bad1613f965473c58d235fefc065252a1ec16
kelley <kelleyc2@illinois.edu>
2019-02-18 23:06:29 -0600

    Add DocumentListItem

---
+++ frontend/src/components/DocumentListItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class DocumentListItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default DocumentListItem
