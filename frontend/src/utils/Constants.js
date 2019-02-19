ab043b3e784c9c9064a1138954de0ca0ae9726f1
Arpan Laha <arpanlaha99@gmail.com>
2019-02-19 00:05:46 -0600

    remove types from constants

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -4,18 +4,6 @@ const Constants = {
     { value: 'Pending' },
     { value: 'Rejected' },
     { value: 'Verified' }
-  ],
-  types: [
-    { value: 'Type 1' },
-    { value: 'Type 2' },
-    { value: 'Type 3' },
-    { value: 'Type 4' },
-    { value: 'Type 5' },
-    { value: 'Type 6' },
-    { value: 'Type 7' },
-    { value: 'Type 8' },
-    { value: 'Type 9' },
-    { value: 'Type 110' }
   ]
 }
 
