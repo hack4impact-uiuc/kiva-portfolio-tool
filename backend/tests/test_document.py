704151b854c0356656b7a6f1712114d2fb0b25bc
Roy Chiu <rychiu13@gmail.com>
2019-02-20 11:45:38 -0600

    lint backend tests

--- backend/tests/test_document.py
+++ backend/tests/test_document.py
@@ -2,16 +2,19 @@ from api.models import db, Document
 from datetime import date
 import enum
 
+
 class MyEnum(enum.Enum):
     one = 1
     two = 2
     three = 3
+
 
 # client passed from client - look into pytest for more info about fixtures
 # test client api: http://flask.pocoo.org/docs/1.0/api/#test-client
 def test_index(client):
     rs = client.get("/")
     assert rs.status_code == 200
+
 
 def test_get_document(client):
     rs = client.get("/documents")
