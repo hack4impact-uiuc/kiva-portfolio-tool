3e32542b26773908bce84a1561d0788a068cab95
Daniel Choi <daniel.won.choi@gmail.com>
2019-02-21 00:12:40 -0600

    bug

--- backend/api/views/message.py
+++ backend/api/views/message.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 from api.models.Message import Message
 from api.core import create_response, serialize_list, logger
 
-business = Blueprint("message", __name__)
+message = Blueprint("message", __name__)
 
 
 @message.route("/messages", methods=["GET"])
