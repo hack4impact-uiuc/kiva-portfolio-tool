1abc04041ca91ac8199c688061f9c768655033c8
Daniel Choi <daniel.won.choi@gmail.com>
2019-02-21 00:10:51 -0600

    wrong branch oops

--- backend/api/views/message.py
+++ backend/api/views/message.py
@@ -3,3 +3,9 @@ from api.models.Message import Message
 from api.core import create_response, serialize_list, logger
 
 business = Blueprint("message", __name__)
+
+
+@message.route("/messages", methods=["GET"])
+def get_messages():
+    messages = Message.query.all()
+    return create_response(data={"messages": serialize_list(messages)})
