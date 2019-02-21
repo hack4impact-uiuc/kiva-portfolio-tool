d9acb0ea732e3c9e1b52361b41fdba02172adefd
Arpan Laha <arpanlaha99@gmail.com>
2019-02-20 23:11:28 -0600

    update mock data

--- frontend/src/utils/MockData.js
+++ frontend/src/utils/MockData.js
@@ -1,48 +1,57 @@
-const MockData = [
-  {
-    status: 'Missing',
-    docType: 'Board of Directors'
-  },
-  {
-    status: 'Missing',
-    docType: 'Auditor Letter'
-  },
-  {
-    status: 'Pending',
-    docType: 'Income Statement',
-    fileName: 'income_statement.pdf',
-    uploadDate: '12/4/2018'
-  },
-  {
-    status: 'Pending',
-    docType: 'Balance Sheet',
-    fileName: 'balance_sheet.pdf',
-    uploadDate: '1/24/2019'
-  },
-  {
-    status: 'Rejected',
-    docType: 'Strategic Plan',
-    fileName: 'strategic_plan.pdf',
-    uploadDate: '12/24/2018'
-  },
-  {
-    status: 'Rejected',
-    docType: 'Annual Plan',
-    fileName: 'annual_plan.pdf',
-    uploadDate: '1/3/2019'
-  },
-  {
-    status: 'Approved',
-    docType: 'Financial Projections',
-    fileName: 'financial_proj.pdf',
-    uploadDate: '12/24/2018'
-  },
-  {
-    status: 'Approved',
-    docType: 'Organizational Chart',
-    fileName: 'org_chard.pdf',
-    uploadDate: '1/23/2019'
-  }
-]
+const MockData = {
+  missing: [
+    {
+      status: 'Missing',
+      docType: 'Board of Directors'
+    },
+    {
+      status: 'Missing',
+      docType: 'Auditor Letter'
+    }
+  ],
+  pending: [
+    {
+      status: 'Pending',
+      docType: 'Income Statement',
+      fileName: 'income_statement.pdf',
+      uploadDate: '12/4/2018'
+    },
+    {
+      status: 'Pending',
+      docType: 'Balance Sheet',
+      fileName: 'balance_sheet.pdf',
+      uploadDate: '1/24/2019'
+    }
+  ],
+
+  rejected: [
+    {
+      status: 'Rejected',
+      docType: 'Strategic Plan',
+      fileName: 'strategic_plan.pdf',
+      uploadDate: '12/24/2018'
+    },
+    {
+      status: 'Rejected',
+      docType: 'Annual Plan',
+      fileName: 'annual_plan.pdf',
+      uploadDate: '1/3/2019'
+    }
+  ],
+  approved: [
+    {
+      status: 'Approved',
+      docType: 'Financial Projections',
+      fileName: 'financial_proj.pdf',
+      uploadDate: '12/24/2018'
+    },
+    {
+      status: 'Approved',
+      docType: 'Organizational Chart',
+      fileName: 'org_chard.pdf',
+      uploadDate: '1/23/2019'
+    }
+  ]
+}
 
 export default MockData
