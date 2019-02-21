e2478a13f92cb7eb6194b0da683c76a60f09babc
Arpan Laha <arpanlaha99@gmail.com>
2019-02-20 23:20:16 -0600

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { DocumentList } from './components'
-import ServiceWorker from './serviceWorker.js'
-//import './styles/htmlstyles.scss'
-
-class ScrollToTop extends Component {
-  /*
-    componentDidUpdate(prevProps) {
-        if (
-            this.props.location !== prevProps.location &&
-            !(
-                this.props.location.pathname === '/results' &&
-                prevProps.location.pathname === '/description'
-            ) &&
-            !(
-                this.props.location.pathname === '/description' &&
-                prevProps.location.pathname === '/results'
-            ) &&
-            !(this.props.location.pathname === '/' && this.props.location.hash !== '')
-        ) {
-            window.scrollTo(0, 0)
-        }
-    }
-    */
-
-  render() {
-    return this.props.children
-  }
-}
 
 ReactDOM.render(
   <Router onUpdate={() => window.scrollTo(0, 0)}>
