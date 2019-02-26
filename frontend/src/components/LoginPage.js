dc2357adb1b22031ad6398289ab9e73fbb46273e
jeffylin <jlin79@illinois.edu>
2019-02-25 22:52:17 -0600

    started making container/redux stuff

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,29 +1,64 @@
 import React, { Component }  from 'react'
-import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
 import { Button } from 'reactstrap'
+import {bindActionCreators} from 'redux'
 
+const mapStateToProps = state => ({
+  verified: state.verification.verified
+})
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      login,
+      beginLoading,
+      endLoading
+    },
+    dispatch
+  )
+}
 
 class LoginPage extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
-			password: ''
+			password: '',
+			email: ''
 		}
 	}
 
-	changedPassword = event => {
+	updatePassword = event => {
 		this.setState({ password: event.target.value })
 	}
+
+	updateEmail = event => {
+		this.setState({ email: event.target.value })
+	}
+
 
 	render() {
 		return (
 			<div>
-				<p>{"Kiva"}</p>
+				<div style={{paddingLeft: '5%'}}
+				>
+					<p>Kiva</p>
+				</div>
+					<span> Email: </span>
+					<input onChange={this.updateEmail} />
+				<br></br>
+					<span> Password: </span>
+					<input onChange={this.updatePassword} />
+				<br></br>
+				<Button type="submit">
+					Sign In
+				</Button>
 			</div>
 		)
 	}
 }
 
-export default LoginPage
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginPage)
