036fbdcdf8ecef994f62ee6ac6b476cdaf11b453
Danielle Yang <dy6@illinois.edu>
2019-02-27 23:01:56 -0600

    Trying to resolve merge conflicts. Again

--- backend/api/models/Document.py
+++ backend/api/models/Document.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from api.core import Mixin
 from .base import db
 
@@ -13,43 +12,6 @@ class Document(Mixin, db.Model):
     fileID = db.Column(db.String, unique=True)
     userID = db.Column(
         db.String, unique=True
-    )  # , db.ForeignKey("user.id",ondelete="SET NULL")
-    date = db.Column(db.DateTime, unique=False)
-    status = db.Column(db.String, unique=False)  # db.Enum
-    docType = db.Column(db.String, unique=False)  # db.Enum
-    docName = db.Column(db.String, unique=False)
-    latest = db.Column(db.Boolean, unique=False)
-    description = db.Column(db.String, unique=False, nullable=True)
-
-    def __init__(
-        self, fileID, userID, date, status, docType, docName, latest, description
-    ):
-        self.fileID = fileID
-        self.userID = userID
-        self.date = date
-        self.status = status
-        self.docType = docType
-        self.docName = docName
-        self.latest = latest
-        self.description = description
-
-    def __repr__(self):
-        return f"<FileID: {self.fileID}>\n <userID: {self.userID}>\n <date: {self.date}>\n <status: {self.status}>\n <docType: {self.docType}>\n <docName {self.docName}>\n <latest {self.latest}>\n <description: {self.description}>\n"
-=======
-from api.core import Mixin
-from .base import db
-
-
-# Note that we use sqlite for our tests, so you can't use Postgres Arrays
-class Document(Mixin, db.Model):
-    """Document Table."""
-
-    __tablename__ = "documents"
-
-    id = db.Column(db.Integer, unique=True, primary_key=True)
-    fileID = db.Column(db.String, unique=True, nullable=True)
-    userID = db.Column(
-        db.String, unique=False
     )  # , db.ForeignKey("user.id",ondelete="SET NULL")
     date = db.Column(db.DateTime, unique=False, nullable=True)
     status = db.Column(db.String, unique=False)  # db.Enum
@@ -72,4 +34,3 @@ class Document(Mixin, db.Model):
 
     def __repr__(self):
         return f"<FileID: {self.fileID}>\n <userID: {self.userID}>\n <date: {self.date}>\n <status: {self.status}>\n <docType: {self.docType}>\n <docName {self.docName}>\n <latest {self.latest}>\n <description: {self.description}>\n"
->>>>>>> master
--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-from flask import Blueprint
-from api.models import Person, Document, Message, FieldPartner, PortfolioManager
-from api.core import create_response, serialize_list, logger
-
-main = Blueprint("main", __name__)  # initialize blueprint
-
-
-# function that is called when you visit /
-@main.route("/")
-def index():
-    # you are now in the current application context with the main.route decorator
-    # access the logger with the logger from api.core and uses the standard logging module
-    # try using ipdb here :) you can inject yourself
-    logger.info("Hello World!")
-    return "<h1>Hello World!</h1>"
-
-
-# function that is called when you visit /persons
-@main.route("/persons", methods=["GET"])
-def get_persons():
-    persons = Person.query.all()
-    return create_response(data={"persons": serialize_list(persons)})
-
-
-# function that is called when you visit /persons
-@main.route("/messages", methods=["GET"])
-def get_messages():
-    messages = Message.query.all()
-    return create_response(data={"messages": serialize_list(messages)})
-
-
-# function that is called when you visit /documetns
-@main.route("/documents", methods=["GET"])
-def get_document():
-    docs = Document.query.all()
-    return create_response(data={"documents": serialize_list(docs)})
-
-# function that is called when you visit /fieldpartners
-@main.route("/field_partner", methods=["GET"])
-def get_field_partner():
-    field_partner = FieldPartner.query.all()
-    return create_response(data={"field_partner": serialize_list(field_partner)})
-
-# function that is called when you visit /portfoliomanager
-@main.route("/portfolio_manager", methods=["GET"])
-def get_portfolio_manager():
-    portfolio_manager = PortfolioManager.query.all()
-    return create_response(data={"portfolio_manager": serialize_list(portfolio_manager)})
-=======
 from flask import Blueprint
 from api.models import Person, Document, Message
 from api.core import create_response, serialize_list, logger
@@ -95,4 +45,3 @@ def add_<yourclassname>():
 def get_document():
     docs = Document.query.all()
     return create_response(data={"documents": serialize_list(docs)})
->>>>>>> master
