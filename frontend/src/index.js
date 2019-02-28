4ca07e2cd548ab92ac25990454cdb8d6d75d8447
kelley <kelleyc2@illinois.edu>
2019-02-27 23:31:24 -0600

    Added bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <Router onUpdate={() => window.scrollTo(0, 0)}>
