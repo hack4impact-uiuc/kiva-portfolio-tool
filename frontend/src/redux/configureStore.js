898788b3d1ad9c70c0ddf727379cfa7791994f35
Arpan Laha <arpanlaha99@gmail.com>
2019-02-28 00:11:24 -0600

    remove local storage stuff

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -4,20 +4,15 @@ import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
 import thunk from 'redux-thunk'
 import reducer from './modules/reducer'
-import { loadState, saveState } from './localStorage'
 
 export const history = createHistory()
-const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const middleware = [thunk, routerMiddleware(history)]
 const composedMiddleware = compose(applyMiddleware(...middleware))
-const persistedState = loadState()
-
-const savePersistedState = store => () => {
-  saveState({ searches: store.getState().results.searches })
-}
 
 export default function configureStore() {
-  let store = createStore(reducer, persistedState, devtools(composedMiddleware))
-  store.subscribe(savePersistedState(store))
-  return store
+  return createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composedMiddleware
+  )
 }
