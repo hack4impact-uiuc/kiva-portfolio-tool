03673013d0a72a97d90c51cb72837d5e97366a7d
Arpan Laha <arpanlaha99@gmail.com>
2019-02-28 22:43:04 -0600

    add default user state

--- frontend/src/redux/modules/reducer.js
+++ frontend/src/redux/modules/reducer.js
@@ -2,10 +2,12 @@ import { routerReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import results from './results'
 import searchpage from './searchpage'
+import user from './user'
 import auth from './auth'
 
 export default combineReducers({
   routing: routerReducer,
+  user,
   results,
   searchpage,
   auth
---
+++ frontend/src/redux/modules/user.js
@@ -0,0 +1,10 @@
+export const user = {
+  isPM: false
+}
+
+export default function reducer(state = searchState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
