d3bb050cb94efed2bc0e564d403791db2d5d8510
Arpan Laha <arpanlaha99@gmail.com>
2019-02-28 23:30:25 -0600

    fix mixup

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -2,7 +2,7 @@ export const user = {
   isPM: false
 }
 
-export default function reducer(state = searchState, action) {
+export default function reducer(state = user, action) {
   switch (action.type) {
     default:
       return state
