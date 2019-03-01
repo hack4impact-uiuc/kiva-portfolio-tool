5d85a3fa53e00424e6e49232f0188c300c3c5966
Arpan Laha <arpanlaha99@gmail.com>
2019-03-01 01:04:51 -0600

    remove childs play redux stuff

--- frontend/src/redux/modules/reducer.js
+++ frontend/src/redux/modules/reducer.js
@@ -1,14 +1,10 @@
 import { routerReducer } from 'react-router-redux'
 import { combineReducers } from 'redux'
-import results from './results'
-import searchpage from './searchpage'
 import user from './user'
 import auth from './auth'
 
 export default combineReducers({
   routing: routerReducer,
   user,
-  results,
-  searchpage,
   auth
 })
