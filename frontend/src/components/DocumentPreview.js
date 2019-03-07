3040fcf0dc015e76bd6cf9878f82a8a604669a95
kelley <kelleyc2@illinois.edu>
2019-03-06 23:14:36 -0600

    Added empty component

---
+++ frontend/src/components/DocumentPreview.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class DocumentPreview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() { 
+    return (<div></div>)
+  }
+}
+
+
+export default DocumentPreview
