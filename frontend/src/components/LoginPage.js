5016a62afae446e9787d9711b651cf70764ea885
Roy Chiu <rychiu13@gmail.com>
2019-03-07 00:38:11 -0600

    format

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Popup } from 'react-popup'
@@ -22,67 +22,65 @@ const mapDispatchToProps = dispatch => {
     },
     dispatch
   )
-} 
-
-
-
-class LoginPage extends Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			password: '',
-			email: '',
-			valid: ['pm@kiva.com', 'fp@kiva.com', 'kiva']
-		}
-	}
-
-	updatePassword = event => {
-		this.setState({ password: event.target.value })
-	}
-
-	updateEmail = event => {
-		this.setState({ email: event.target.value })
-	}
-
-	verify = event => {
-		/* this.props.beginLoading() */
-		this.props.login(this.state.valid.indexOf(this.state.email) > -1 && this.state.valid.indexOf(this.state.password) > -1)
-		this.props.setUserType(this.state.email === "pm@kiva.com" && this.state.password === "kiva")
-		if (this.props.verified === true) {
-			this.props.history.push('/dashboard')
-		}
-		/* else{
-			alert('Email or password is invalid.\nPlease try again.')
-		} */
-
-	}
-
-	render() {
-		return (
-			<div>
-				<div style={{paddingLeft: '5%'}}
-				>
-					<p>Kiva</p>
-				</div>
-				<form onSubmit={this.verify}>
-					<span> Email: </span>
-					<input onChange={this.updateEmail} />
-					<br></br>
-					<span> Password: </span>
-					<input onChange={this.updatePassword} />
-					<br></br>
-					<Button type="submit" onClick={this.verify}>
-						Sign In
-					</Button>
-				</form>
-			</div>
-		)
-	}
 }
 
+class LoginPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      password: '',
+      email: '',
+      valid: ['pm@kiva.com', 'fp@kiva.com', 'kiva']
+    }
+  }
+
+  updatePassword = event => {
+    this.setState({ password: event.target.value })
+  }
+
+  updateEmail = event => {
+    this.setState({ email: event.target.value })
+  }
+
+  verify = event => {
+    /* this.props.beginLoading() */
+    this.props.login(
+      this.state.valid.indexOf(this.state.email) > -1 &&
+        this.state.valid.indexOf(this.state.password) > -1
+    )
+    this.props.setUserType(this.state.email === 'pm@kiva.com' && this.state.password === 'kiva')
+    if (this.props.verified === true) {
+      this.props.history.push('/dashboard')
+    }
+    /* else{
+			alert('Email or password is invalid.\nPlease try again.')
+		} */
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ paddingLeft: '5%' }}>
+          <p>Kiva</p>
+        </div>
+        <form onSubmit={this.verify}>
+          <span> Email: </span>
+          <input onChange={this.updateEmail} />
+          <br />
+          <span> Password: </span>
+          <input onChange={this.updatePassword} />
+          <br />
+          <Button type="submit" onClick={this.verify}>
+            Sign In
+          </Button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(LoginPage)
