e50a815560e57ea13f1b7704541d8810053c4126
Roy Chiu <rychiu13@gmail.com>
2019-03-07 00:56:44 -0600

    format

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -43,17 +43,19 @@ class LoginPage extends Component {
     this.setState({ email: event.target.value })
   }
 
-	verify = event => {
-
-		// This should check if the email and password are valid (will eventually look into email/password database)
-		this.props.login(this.state.valid.indexOf(this.state.email) > -1 && this.state.valid.indexOf(this.state.password) > -1)
-		// This should look at email/password to determine if they are a Portfolio Manager or Field Partner
-		this.props.setUserType(this.state.email === "pm@kiva.com" && this.state.password === "kiva")
-		// If login was successful, then bring user to dashboard
-		if (this.props.verified === true) {
-			this.props.history.push('/dashboard')
-		}
-		/* else{
+  verify = event => {
+    // This should check if the email and password are valid (will eventually look into email/password database)
+    this.props.login(
+      this.state.valid.indexOf(this.state.email) > -1 &&
+        this.state.valid.indexOf(this.state.password) > -1
+    )
+    // This should look at email/password to determine if they are a Portfolio Manager or Field Partner
+    this.props.setUserType(this.state.email === 'pm@kiva.com' && this.state.password === 'kiva')
+    // If login was successful, then bring user to dashboard
+    if (this.props.verified === true) {
+      this.props.history.push('/dashboard')
+    }
+    /* else{
 			alert('Email or password is invalid.\nPlease try again.')
 		} */
   }
