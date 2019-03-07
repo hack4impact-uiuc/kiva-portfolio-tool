159f676e79185a1c71b607d60955a0868c504356
jeffylin <jlin79@illinois.edu>
2019-03-07 00:52:25 -0600

    added some comments in verify

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -46,9 +46,12 @@ class LoginPage extends Component {
 	}
 
 	verify = event => {
-		/* this.props.beginLoading() */
+
+		// This should check if the email and password are valid (will eventually look into email/password database)
 		this.props.login(this.state.valid.indexOf(this.state.email) > -1 && this.state.valid.indexOf(this.state.password) > -1)
+		// This should look at email/password to determine if they are a Portfolio Manager or Field Partner
 		this.props.setUserType(this.state.email === "pm@kiva.com" && this.state.password === "kiva")
+		// If login was successful, then bring user to dashboard
 		if (this.props.verified === true) {
 			this.props.history.push('/dashboard')
 		}
