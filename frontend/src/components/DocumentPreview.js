96364d2bf3ce6582bc102481c08841521fbc09e7
Arpan Laha <arpanlaha99@gmail.com>
2019-03-10 20:24:02 -0500

    merge

--- frontend/src/components/DocumentPreview.js
+++ frontend/src/components/DocumentPreview.js
@@ -2,33 +2,21 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import { ContentPreview } from 'box-ui-elements'
-<<<<<<< HEAD
 import { getAccessToken, updateDocumentStatus } from '../utils/ApiWrapper'
 import 'box-ui-elements/dist/preview.css'
-=======
-import { IntlProvider } from 'react-intl'
-import { getAccessToken, updateDocumentStatus } from '../utils/ApiWrapper'
->>>>>>> 0e71157e72743a8f62ee9597c52943fe88473c37
 
 const mapStateToProps = state => ({
   isPM: state.user.isPM
 })
 
-<<<<<<< HEAD
 const container = document.querySelector('.container')
-=======
->>>>>>> 0e71157e72743a8f62ee9597c52943fe88473c37
 class DocumentPreview extends Component {
   constructor(props) {
     super(props)
     this.state = {
       id: this.props.document._id,
       fileName: this.props.document.fileName,
-<<<<<<< HEAD
       fileId: 419072325627,
-=======
-      fileId: 418675740558,
->>>>>>> 0e71157e72743a8f62ee9597c52943fe88473c37
       accessToken: null
     }
 
@@ -68,7 +56,6 @@ class DocumentPreview extends Component {
 
   render() {
     const { isPM } = this.props
-<<<<<<< HEAD
 
     return (
       (
@@ -88,37 +75,6 @@ class DocumentPreview extends Component {
             language={'en-US'}
             messages={'messages'}
           />
-=======
-    return (
-      <>
-        {this.state.fileName && (
-          <Button color="primary" onClick={this.toggle}>
-            {isPM ? 'REVIEW' : 'VIEW'}
-          </Button>
-        )}
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader>{this.state.fileName}</ModalHeader>
-          <ModalBody>
-            <IntlProvider locale="en" textComponent={React.Fragment}>
-              <ContentPreview
-                contentSidebarProps={{
-                  detailsSidebarProps: {
-                    hasAccessStats: false,
-                    hasClassification: false,
-                    hasNotices: false,
-                    hasProperties: false,
-                    hasRetentionPolicy: false,
-                    hasVersions: true
-                  },
-                  hasActivityFeed: false,
-                  hasMetadata: false,
-                  hasSkills: false
-                }}
-                fileId={this.state.fileId}
-                token={this.state.accessToken}
-              />
-            </IntlProvider>
->>>>>>> 0e71157e72743a8f62ee9597c52943fe88473c37
           </ModalBody>
           <ModalFooter>
             {isPM && (
@@ -136,12 +92,8 @@ class DocumentPreview extends Component {
             </Button>
           </ModalFooter>
         </Modal>
-<<<<<<< HEAD
         </>
       )
-=======
-      </>
->>>>>>> 0e71157e72743a8f62ee9597c52943fe88473c37
     )
   }
 }
