5cfec5b36497443408429670ce24c4aad35a7ddd
kelley <kelleyc2@illinois.edu>
2019-03-26 00:40:44 -0500

    Added DocumentClass model

---
+++ backend/api/models/DocumentClass.py
@@ -0,0 +1,24 @@
+from api.core import Mixin
+from .base import db
+
+
+# Note that we use sqlite for our tests, so you can't use Postgres Arrays
+class Document(Mixin, db.Model):
+    """Document Class Table."""
+
+    __tablename__ = "document_class"
+
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String, unique=False, nullable=True)
+
+    def __init__(
+        self,
+        name,
+        description=None,
+    ):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f"<name: {self.name}>\n <description: {self.description}>\n"
