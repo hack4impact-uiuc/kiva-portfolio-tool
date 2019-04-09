9774c42ff1b8f598d363940bd60748ec5f036b6e
Daniel <daniel.won.choi@gmail.com>
2019-04-08 23:21:52 -0500

    formatted

--- frontend/src/components/DocumentPreview.js
+++ frontend/src/components/DocumentPreview.js
@@ -75,15 +75,9 @@ class DocumentPreview extends Component {
     const { isPM } = this.props
 
     const customStyles = {
-      // top: '50%',
-      // left: '50%',
-      // right: 'auto',
-      // bottom: 'auto',
-      // marginRight: '-50%',
-      // transform: 'translate(-50%, -50%)',
-      height: '500px', // <-- This sets the height
+      height: '500px',
       width: '500px',
-      overlfow: 'scroll' // <-- This tells the modal to scrol
+      overlfow: 'scroll'
     }
 
     return (
