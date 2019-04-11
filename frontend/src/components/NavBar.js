da4809503ee1895c5d687a6225476946e4f15deb
Danielle Yang <dy6@illinois.edu>
2019-04-11 12:12:36 -0500

    Fixed navbar item spacing

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -15,6 +15,8 @@ import { connect } from 'react-redux'
 import kiva_logo from '../media/kivaPlainLogo.png'
 import notification_bell from '../media/notificationBell.png'
 import info_image from '../media/info.svg'
+import menu_image from '../media/menu.png'
+import "../styles/navbar.css"
 
 const mapStateToProps = state => ({
   isPM: state.user.isPM
@@ -38,14 +40,14 @@ class NavBar extends Component {
 
             <NavItem>
               <Button color="clear">
-                <img src={info_image} width="30" height="30" />
+                <img src={info_image} width="29" height="29" />
               </Button>
             </NavItem>
 
-            <NavItem>
+            <NavItem className="menu">
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                  O
+                  <img src={menu_image} width="30" height="35"/>
                 </DropdownToggle>
                 <DropdownMenu right>
                   {isPM && <DropdownItem>Manage Documents</DropdownItem>}
