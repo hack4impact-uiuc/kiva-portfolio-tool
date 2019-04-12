dee8f86ad96a6f9c6eac261245c453afb8b59328
Daniel <daniel.won.choi@gmail.com>
2019-04-12 10:14:03 -0500

    change

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,4 +1,4 @@
-//import React, { Component } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Button } from 'reactstrap'
 import { bindActionCreators } from 'redux'
