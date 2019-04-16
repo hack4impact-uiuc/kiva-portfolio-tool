97d1f8007e16f916c061cd2572e90645965aeb05
Arpan Laha <arpanlaha99@gmail.com>
2019-04-15 23:13:42 -0500

    initial document class page stuff

--- frontend/src/components/DocumentClassPage.js
+++ frontend/src/components/DocumentClassPage.js
@@ -1,46 +1,59 @@
 import React from 'react'
 import DocumentClassList from './DocumentClassList'
 import { getAllDocumentClasses } from '../utils/ApiWrapper'
-import { Container, Row, Col } from 'reactstrap'
+import { Button, Modal, ModalFooter } from 'reactstrap'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import '../styles/dashboard.css'
-import { updateDocuments } from '../redux/modules/user'
+import { updateDocumentClasses } from '../redux/modules/user'
 import '../styles/index.css'
+
+const mapStateToProps = state => ({
+    documentClasses: state.user.documentClasses
+})
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators(
+        {
+            updateDocumentClasses
+        },
+        dispatch
+    )
+}
 
 class DocumentClassPage extends React.Component {
     constructor(props) {
         super(props)
-
         this.state = {
-            documentClasses: []
+            modalOpen: false
         }
+        this.toggle = this.toggle.bind(this)
     }
 
     async componentDidMount() {
         const res = await getAllDocumentClasses()
         if (res) {
-            this.setState({ documentClasses: res })
+            this.updateDocumentClasses(res)
         } else {
-            this.setState({ documentClasses: [] })
+            this.updateDocumentClasses([])
         }
+    }
+
+    toggle() {
+        this.setState({ modalOpen: !this.state.modalOpen })
     }
 
     render() {
         return (
-            <Container>
-                <Row>
-                    {this.props.documents
-                        ? this.state.statuses.map(key => {
-                            return (
-                                <Col sm="12" md="6">
-                                    <DocumentList documents={this.props.documents[key]} status={key} />
-                                </Col>
-                            )
-                        })
-                        : null}
-                </Row>
-            </Container>
+            <>
+                <h1>Edit Document Classes</h1>
+                <Button color="primary" onClick={this.toggle}>
+                    Add New Document Class
+                </Button>
+                <DocumentClassList documentClasses={this.props.documentClasses} />
+            </>
         )
     }
 }
 
-export default DocumentClassPage
+export default connect (mapStateToProps, mapDispatchToProps) (DocumentClassPage)
