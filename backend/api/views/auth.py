06b9080f12eef0ebdd5b1ab4eccaf39bd45ccdaa
Daniel <daniel.won.choi@gmail.com>
2019-04-23 17:53:57 -0500

    working on endpoints

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -2,13 +2,14 @@ from flask import Blueprint, request, json
 from api.models import DocumentClass, db
 from api.core import create_response, serialize_list, logger
 
-import requests, json
+import requests, json, random, string
 
 auth = Blueprint("auth", __name__)
 
+backend_url = "http://localhost:8000/"
+
 @auth.route("/register", methods=["POST"])
 def register_user():
-    print("asdf")
     data = request.get_json()
     if data is None:
         data = request.form
@@ -17,8 +18,92 @@ def register_user():
     password = data["password"]
     role = data["role"]
     
-    r = requests.post("http://localhost:8000/register", data={'email': email, 'password': password, 'role': role})
+    r = requests.post(backend_url + "register", data={'email': email, 'password': password, 'role': role})
 
     return create_response(status=200, message="success", data={'token':r.text.token, 'uid': r.text.uid})
 
-#@auth.route("/login", methods=["GET"])
+@auth.route("/verifyEmail", methods=["POST"])
+def verify_email():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+
+    email = data["email"]
+    pin = data["pin"]
+
+    r = requests.post(backend_url + "verifyEmail", data={'email': email, 'pin': pin})
+
+    return create_response(status=200, message="success")
+
+@auth.route("/createFP", methods=["POST"])
+def create_fp():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+
+    email = data["email"]
+    password = randomStringDigits()
+    role = "fp"
+
+    r = requests.post(backend_url + "register", data={'email': email, 'password': password, 'role': role})
+
+    return create_response(status=200, message="success", data={'token':r.text.token, 'uid': r.text.uid, 'password': password})
+
+def randomStringDigits():
+    string_length = 10
+
+    lettersAndDigits = string.ascii_letters + string.digits
+    return ''.join(random.choice(lettersAndDigits) for i in range(string_length))
+
+
+@auth.route("/changePassword", methods=["POST"])
+def change_password():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+
+    token = data["token"]
+    current_password = data["currentPassword"]
+    new_password = data["newPassword"]
+
+    r = requests.post(backend_url + "changePassword", data={'token': token, 'currentPassword': curr})
+
+    return create_response(status=200, message="success", data={'token': r.text.token})
+
+@auth.route("/forgotPassword", methods=["POST"])
+def forgot_password():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+
+    email = data["email"]
+    answer = data["answer"]
+
+    r = requests.post(backend_url + "forgotPassword", data={'email': email, 'answer': answer})
+
+    return create_response(status=200, message=r.text.message)
+
+@auth.route("/resetPassword", methods=["POST"])
+def reset_password():
+    data = request.get_json()
+    if data is None:
+        data = request.form
+
+    email = data["email"]
+    password = data["password"]
+    pin = data["pin"]
+    answer = data["answer"]
+
+    r = requests.post(backend_url + "resetPassword", data={'email': email, 'password': password, 'pin': pin, 'answer': answer})
+
+    return create_response(status=200, message="success", data={'token': r.text.token})
+
+@auth.route("/addSecurityQuestion", methods=["POST"])
+def add_security_question():
+
+@auth.route("/getSecurityQuestion", methods=["POST"])
+def get_security_question():
+
+@auth.route("/resendVerification", methods=["POST"])
+def resend_verification():
+
