523b54966ec944490f69e67475d0bb378a08af38
Roy Chiu <rychiu13@gmail.com>
2019-04-27 02:41:29 -0500

    updated document class page

--- frontend/src/components/DocumentClassPage.js
+++ frontend/src/components/DocumentClassPage.js
@@ -94,97 +94,85 @@ class DocumentClassPage extends React.Component {
   }
 
   render() {
-    if (this.props.loading) {
-      return (
-        <div
-          className="resultsText"
-          style={{ paddingTop: window.innerWidth >= 550 ? '10%' : '20%' }}
-        >
-          Loading
-          <Loader type="Puff" color="green" height="100" width="100" />
-        </div>
-      )
-    } else {
-      return (
-        <>
-          <NavBar />
-          <Modal isOpen={this.state.addModal} toggle={this.toggle}>
-            <ModalBody>
-              <form>
-                <span> Name: </span>
-                <input onChange={this.updateName} />
-                <br />
-                <span> Description: </span>
-                <textarea
-                  name="paragraph_text"
-                  cols="50"
-                  rows="10"
-                  onChange={this.updateDescription}
-                />
-                <br />
-                <div className="dropPage">
-                  <section className="droppedBox">
-                    <div className="dropZone">
-                      <Dropzone onDrop={this.onDrop.bind(this)}>
-                        {({ getRootProps, getInputProps }) => (
-                          <section>
-                            <div {...getRootProps()}>
-                              <input {...getInputProps()} />
-                              {this.state.files.length > 0 ? (
-                                <p>File uploaded: {this.state.files[0].name}</p>
-                              ) : (
-                                <p>Click to Upload</p>
-                              )}
-                            </div>
-                          </section>
-                        )}
-                      </Dropzone>
-                    </div>
-                  </section>
-                  <hr />
-                </div>
-              </form>
-            </ModalBody>
-            <ModalFooter>
-              <Button className="invalidSearchButton" onClick={this.toggle}>
-                Return
-              </Button>
-              <Button disabled={this.state.files.length === 0} onClick={this.handleSubmit}>
-                Create Document Class
-              </Button>
-            </ModalFooter>
-          </Modal>
-          <div className="edit-banner">
-            <h1 className="h1">Edit Document Classes</h1>
-            <Button
-              className="add-doc-text"
-              id="add-new-class"
-              color="transparent"
-              onClick={this.toggle}
-            >
-              <img className="addImg" src={add} />
-              <span className="add-doc-text">Add New Document Class</span>
+    return (
+      <>
+        <NavBar />
+        <Modal isOpen={this.state.addModal} toggle={this.toggle}>
+          <ModalBody>
+            <form>
+              <span> Name: </span>
+              <input onChange={this.updateName} />
+              <br />
+              <span> Description: </span>
+              <textarea
+                name="paragraph_text"
+                cols="50"
+                rows="10"
+                onChange={this.updateDescription}
+              />
+              <br />
+              <div className="dropPage">
+                <section className="droppedBox">
+                  <div className="dropZone">
+                    <Dropzone onDrop={this.onDrop.bind(this)}>
+                      {({ getRootProps, getInputProps }) => (
+                        <section>
+                          <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {this.state.files.length > 0 ? (
+                              <p>File uploaded: {this.state.files[0].name}</p>
+                            ) : (
+                              <p>Click to Upload</p>
+                            )}
+                          </div>
+                        </section>
+                      )}
+                    </Dropzone>
+                  </div>
+                </section>
+                <hr />
+              </div>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button className="invalidSearchButton" onClick={this.toggle}>
+              Return
             </Button>
-          </div>
+            <Button disabled={this.state.files.length === 0} onClick={this.handleSubmit}>
+              Create Document Class
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <div className="edit-banner">
+          <h1 className="h1">Edit Document Classes</h1>
+          <Button
+            className="add-doc-text"
+            id="add-new-class"
+            color="transparent"
+            onClick={this.toggle}
+          >
+            <img className="addImg" src={add} />
+            <span className="add-doc-text">Add New Document Class</span>
+          </Button>
+        </div>
 
-          <div className="doc-table">
-            <Table>
-              <tbody>
-                <tr id="doc-table-top-row">
-                  <th className="theader-centered">Document Class Name</th>
-                  <th />
-                </tr>
-                {this.props.documentClasses
-                  ? this.props.documentClasses.map(documentClass => (
-                      <DocumentClass documentClass={documentClass} />
-                    ))
-                  : null}
-              </tbody>
-            </Table>
-          </div>
-        </>
-      )
-    }
+        <div className="doc-table">
+          <Table>
+            <tbody>
+              <tr id="doc-table-top-row">
+                <th className="theader-centered">Document Class Name</th>
+                <th />
+              </tr>
+              {this.props.documentClasses
+                ? this.props.documentClasses.map(documentClass => (
+                    <DocumentClass documentClass={documentClass} />
+                  ))
+                : null}
+            </tbody>
+          </Table>
+        </div>
+      </>
+    )
   }
 }
 
