7e648235b037b06e0ba7f6da5d294da3183a6435
Navam <=>
2019-04-28 02:41:59 -0500

    Forgot Passwords done

---
+++ frontend/src/tests/ForgotPassword.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {ForgotPassword} from '../components/ForgotPassword'
+import { Form, Button, FormGroup, Label, Input, Card, Alert, CardBody, CardTitle } from 'reactstrap'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallowToJson, mountToJson } from 'enzyme-to-json'
+import { configure, shallow, mount } from 'enzyme'
+import chai from 'chai'
+global.jestExpect = global.expect;
+global.expect = chai.expect;
+import sinon from 'sinon';
+
+
+beforeAll(() => {
+    configure({ adapter: new Adapter() })
+  })
+
+test(' renders correctly', () => {
+    const component = shallow(<ForgotPassword/>)
+
+    let tree = shallowToJson(component)
+    jestExpect(tree).toMatchSnapshot()
+})
+
+describe('< />', () => {
+    it('renders All major components', () => {
+      const wrapper = shallow(<ForgotPassword/>);
+      expect(wrapper.find(Form)).to.have.lengthOf(1);
+      expect(wrapper.find(Button)).to.have.lengthOf(1);
+      expect(wrapper.find(FormGroup)).to.have.lengthOf(1);
+      expect(wrapper.find(Input)).to.have.lengthOf(1);
+      expect(wrapper.find(Card)).to.have.lengthOf(1);
+      expect(wrapper.find(CardBody)).to.have.lengthOf(1);
+      expect(wrapper.find(CardTitle)).to.have.lengthOf(1);
+
+    });
+  
+    it('renders css', () => {
+        const wrapper = shallow(<ForgotPassword/>);
+        expect(wrapper.find('.nav-absolute')).to.have.lengthOf(1);
+      expect(wrapper.find('.background')).to.have.lengthOf(1);
+      expect(wrapper.find('.foreground')).to.have.lengthOf(1);
+      expect(wrapper.find('.interview-card')).to.have.lengthOf(1);
+      expect(wrapper.find('.text-centered')).to.have.lengthOf(2);
+
+    });
+  
+  });
