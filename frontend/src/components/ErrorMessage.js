c890b0161a86ca1c962ca7024ceb7133871fa503
jeffylin <jlin79@illinois.edu>
2019-05-03 18:44:16 -0500

    pulled master to fix frontend test

--- frontend/src/components/ErrorMessage.js
+++ frontend/src/components/ErrorMessage.js
@@ -4,9 +4,8 @@ export class ErrorMessage extends Component {
   render() {
     return (
       <div>
-        <h3 style={{textAlign: 'center', padding: 150}}>
-          ERROR 404.
-          This page could not be found. :(
+        <h3 style={{ textAlign: 'center', padding: 150 }}>
+          ERROR 404. This page could not be found. :(
         </h3>
       </div>
     )
