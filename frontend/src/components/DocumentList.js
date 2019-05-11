bf3aa749b345d1912014de6f2219c9878ea74f1a
Navam <=>
2019-05-11 14:56:13 -0500

    formatted

--- frontend/src/components/DocumentList.js
+++ frontend/src/components/DocumentList.js
@@ -11,12 +11,12 @@ export class DocumentList extends Component {
 
     this.state = {
       expanded: false,
-      showLess: 5, 
+      showLess: 5
     }
   }
 
   toggleExpand = () => {
-    this.setState({expanded: !this.state.expanded})
+    this.setState({ expanded: !this.state.expanded })
     // console.log(this.props.documents.length)
   }
 
@@ -42,41 +42,49 @@ export class DocumentList extends Component {
             <th />
           </tr>
           {this.props.documents
-            ? this.state.expanded ? this.props.documents.map(document => (
-                <DocumentListItem
-                  document={document}
-                  docClass={document.docClass}
-                  fileName={document.fileName}
-                  fileId={document.fileId}
-                />
-              ))
-              : this.props.documents.slice(0,this.state.showLess).map(document => (
-                <DocumentListItem
-                  document={document}
-                  docClass={document.docClass}
-                  fileName={document.fileName}
-                  fileId={document.fileId}
-                />
-              ))
+            ? this.state.expanded
+              ? this.props.documents.map(document => (
+                  <DocumentListItem
+                    document={document}
+                    docClass={document.docClass}
+                    fileName={document.fileName}
+                    fileId={document.fileId}
+                  />
+                ))
+              : this.props.documents
+                  .slice(0, this.state.showLess)
+                  .map(document => (
+                    <DocumentListItem
+                      document={document}
+                      docClass={document.docClass}
+                      fileName={document.fileName}
+                      fileId={document.fileId}
+                    />
+                  ))
             : null}
-          { this.props.documents
-              ? this.props.documents.length > this.state.showLess
-                ? <tr className='text-centered'>
-                    <td colSpan="5" id="buttonRowData">
-                      {this.state.expanded
-                        ? <button className={'expandButton ' + this.props.status + 'Color'} onClick={this.toggleExpand}>
-                            <img className='expandButtonImage' src={collapse}/>
-                          </button>
-                        : <button className={'expandButton ' + this.props.status + 'Color'} onClick={this.toggleExpand}>
-                            <img className='expandButtonImage' src={expand}/>
-                          </button>
-                      }
-                    </td>
-                  </tr>
-                : null
-              : null
-          }
-          
+          {this.props.documents ? (
+            this.props.documents.length > this.state.showLess ? (
+              <tr className="text-centered">
+                <td colSpan="5" id="buttonRowData">
+                  {this.state.expanded ? (
+                    <button
+                      className={'expandButton ' + this.props.status + 'Color'}
+                      onClick={this.toggleExpand}
+                    >
+                      <img className="expandButtonImage" src={collapse} />
+                    </button>
+                  ) : (
+                    <button
+                      className={'expandButton ' + this.props.status + 'Color'}
+                      onClick={this.toggleExpand}
+                    >
+                      <img className="expandButtonImage" src={expand} />
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ) : null
+          ) : null}
         </tbody>
       </Table>
     )
