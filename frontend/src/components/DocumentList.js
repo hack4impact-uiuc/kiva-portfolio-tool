1032eb55b0fd42890ba5de45f8ee92c08b193d30
Navam <=>
2019-05-11 14:55:01 -0500

    Expandible component created

--- frontend/src/components/DocumentList.js
+++ frontend/src/components/DocumentList.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react'
 import { Table } from 'reactstrap'
 import DocumentListItem from './DocumentListItem'
 import '../styles/documentlist.css'
+import expand from '../media/expand.png'
+import collapse from '../media/collapse.png'
 
 export class DocumentList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      expanded: false,
+      showLess: 5, 
+    }
+  }
+
+  toggleExpand = () => {
+    this.setState({expanded: !this.state.expanded})
+    // console.log(this.props.documents.length)
+  }
+
   render() {
     return (
       <Table>
@@ -26,7 +42,15 @@ export class DocumentList extends Component {
             <th />
           </tr>
           {this.props.documents
-            ? this.props.documents.map(document => (
+            ? this.state.expanded ? this.props.documents.map(document => (
+                <DocumentListItem
+                  document={document}
+                  docClass={document.docClass}
+                  fileName={document.fileName}
+                  fileId={document.fileId}
+                />
+              ))
+              : this.props.documents.slice(0,this.state.showLess).map(document => (
                 <DocumentListItem
                   document={document}
                   docClass={document.docClass}
@@ -35,6 +59,24 @@ export class DocumentList extends Component {
                 />
               ))
             : null}
+          { this.props.documents
+              ? this.props.documents.length > this.state.showLess
+                ? <tr className='text-centered'>
+                    <td colSpan="5" id="buttonRowData">
+                      {this.state.expanded
+                        ? <button className={'expandButton ' + this.props.status + 'Color'} onClick={this.toggleExpand}>
+                            <img className='expandButtonImage' src={collapse}/>
+                          </button>
+                        : <button className={'expandButton ' + this.props.status + 'Color'} onClick={this.toggleExpand}>
+                            <img className='expandButtonImage' src={expand}/>
+                          </button>
+                      }
+                    </td>
+                  </tr>
+                : null
+              : null
+          }
+          
         </tbody>
       </Table>
     )
