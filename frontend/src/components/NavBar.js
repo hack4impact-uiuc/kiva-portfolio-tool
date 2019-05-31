e2a61eeba6a8f64a6666762c94e0642a68a415d8
Daniel Choi <daniel.won.choi@gmail.com>
2019-05-30 23:13:57 -0700

    fixed bugs

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -23,6 +23,7 @@ import kiva_logo from '../media/kivaPlainLogo.png'
 import info_image from '../media/gray_info.png'
 import sandwich_image from '../media/sandwich.png'
 import { removeCookie } from '../utils/cookie'
+import { getUser, getFPByEmail, getPMByEmail } from '../utils/ApiWrapper'
 
 import '../styles/index.css'
 import '../styles/navbar.css'
@@ -46,7 +47,11 @@ export class NavBar extends Component {
     this.state = {
       isLoginPage: null,
       sidebarOpen: false,
-      sidebarClass: sidebarClassName[0]
+      sidebarClass: sidebarClassName[0],
+      role: '',
+      email: '',
+      wrongInfo: '',
+      errorMessage: ''
     }
 
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
@@ -67,7 +72,17 @@ export class NavBar extends Component {
     this.props.history.push('/')
   }
 
-  componentDidMount() {
+  redirect = async e => {
+    if (this.state.role === 'fp') {
+      let fp = await getFPByEmail(this.state.email)
+      this.props.history.push('/dashboard/fp/' + fp._id)
+    } else {
+      let pm = await getPMByEmail(this.state.email)
+      this.props.history.push('/overview/' + pm._id)
+    }
+  }
+
+  async componentDidMount() {
     if (
       this.props.location.pathname !== '/' &&
       this.props.location.pathname !== '/register' &&
@@ -78,6 +93,23 @@ export class NavBar extends Component {
     } else {
       this.setState({ isLoginPage: true })
     }
+
+    const result = await getUser()
+    if (
+      result.error != null &&
+      (result.error.response.status === 400 || result.error.response.status === 500)
+    ) {
+      this.setState({
+        wrongInfo: !this.state.wrongInfo,
+        errorMessage: result.error.response.data.message
+      })
+      return
+    }
+
+    this.setState({
+      role: result.response.data.result.userRole,
+      email: result.response.data.result.email
+    })
   }
 
   render() {
@@ -131,7 +163,7 @@ export class NavBar extends Component {
           )}
 
           {!this.state.isLoginPage && (
-            <NavbarBrand href="/">
+            <NavbarBrand onClick={this.redirect}>
               <img src={kiva_logo} width="90" height="50" alt="Kiva logo" />
             </NavbarBrand>
           )}
@@ -151,11 +183,13 @@ export class NavBar extends Component {
                   </DropdownToggle>
                   <DropdownMenu right>
                     {isPM && (
-                      <DropdownItem onClick={() => this.props.history.push('/documentclasses')}>
-                        Manage Documents
-                      </DropdownItem>
+                      <div>
+                        <DropdownItem onClick={() => this.props.history.push('/documentclasses')}>
+                          Manage Documents
+                        </DropdownItem>
+                        <DropdownItem onClick={this.redirect}>Dashboard</DropdownItem>
+                      </div>
                     )}
-                    <DropdownItem onClick={this.logout}>Log Out</DropdownItem>
                     <DropdownItem onClick={() => this.props.history.push('/changePassword')}>
                       Change Password
                     </DropdownItem>
@@ -164,6 +198,7 @@ export class NavBar extends Component {
                     >
                       Change Security Question
                     </DropdownItem>
+                    <DropdownItem onClick={this.logout}>Log Out</DropdownItem>
                   </DropdownMenu>
                 </UncontrolledDropdown>
               </NavItem>
