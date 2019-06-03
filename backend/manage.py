7108436faa56bfa72e70cbfddea04ce90828f6d9
Kelley Chau <ky.cu303@gmail.com>
2019-06-03 14:43:49 -0400

    Implementing due dates (#193)
    * implemented due date setting in selectdocuments and fp table
    
    * BigInt in db, setting works
    
    * Displaying due date
    
    * Testing updates
    
    * Frontend tests
    
    * Some of backend test
    
    * I forgot to update frontend ig
    
    * Hopefully all done
    
    * Frontend test again
    
    * Date on PMMainPage
    
    * Shorten year in PM page
    
    * npm run format
    
    * style dashboard due date
    
    * style pm dashboard duedate
    
    * finish styling

--- backend/manage.py
+++ backend/manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from api import create_app
 from api.models import db, Document, FieldPartner, PortfolioManager, DocumentClass
-from datetime import datetime
+import time
 
 # sets up the app
 app = create_app()
@@ -39,11 +39,17 @@ def recreate_db():
 
     db.session.commit()
 
-    fp1_id = create_mock_fp("fp1@kiva.org", "FP 1", "In Process", pm_id)
+    date = int(time.time()) * 1000
 
-    fp2_id = create_mock_fp("fp2@kiva.org", "FP 2", "New Partner", pm_id)
+    fp1_id = create_mock_fp(
+        "fp1@kiva.org", "FP 1", "In Process", pm_id, date + 30000000
+    )
 
-    fp3_id = create_mock_fp("fp3@kiva.org", "FP 3", "Complete", pm_id)
+    fp2_id = create_mock_fp(
+        "fp2@kiva.org", "FP 2", "New Partner", pm_id, date + 3000000
+    )
+
+    fp3_id = create_mock_fp("fp3@kiva.org", "FP 3", "Complete", pm_id, date + 10000000)
 
     db.session.commit()
 
@@ -185,9 +191,15 @@ def create_mock_pm(email, name):
     return pm.id
 
 
-def create_mock_fp(email, org_name, app_status, pm_id):
+def create_mock_fp(email, org_name, app_status, pm_id, due_date):
     fp = FieldPartner(
-        {"email": email, "org_name": org_name, "app_status": app_status, "pm_id": pm_id}
+        {
+            "email": email,
+            "org_name": org_name,
+            "app_status": app_status,
+            "pm_id": pm_id,
+            "due_date": due_date,
+        }
     )
     db.session.add(fp)
     return fp.id
